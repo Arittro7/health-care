f8097f6a625945106dc45322fcf4f78a47478ae9
Arittro7 <arittrowpdev@gmail.com>
2025-11-21 22:14:26 +0600

    68-8 Sidebar structured

--- src/components/modules/Dashboard/DashboardSidebar.tsx
+++ src/components/modules/Dashboard/DashboardSidebar.tsx
@@ -1,10 +1,17 @@
 import { getUserInfo } from "@/services/auth/getUserInfo"
 import { UserInfo } from "@/types/user.interface"
 import DashboardSidebarContent from "./DashboardSidebarContent"
+import { getDefaultDashboardRoute } from "@/lib/auth-utils"
+import { navSection } from "@/types/dashboard.interface"
 
 const DashboardSidebar = async () => {
   const userInfo = (await getUserInfo()) as UserInfo
-  return <DashboardSidebarContent userInfo={userInfo} />
+
+  const navItems : navSection[] = []//later on the dynamic role based functionality will be added
+
+  const dashboardHome = getDefaultDashboardRoute(userInfo.role)
+
+  return <DashboardSidebarContent userInfo={userInfo} navItems={navItems} dashboardHome={dashboardHome} />
 }
 
 export default DashboardSidebar
@@ -15,5 +22,7 @@ Same as Navbar I will create a client component file for sidebar content.
 1. I will make this a async-await component and copy-paste the UserInfo as Dashboard navbar
 2. Call the DashboardSidebarContent and pass the userInfo as props
 3. After Passing the props I will start coding on DashboardSidebarContent
+
+4. Declare NavItems [add navSection as type] and Dashboard Home Variable and pass as props to DashboardSidebarContent, 
 
 */
